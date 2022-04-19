312c0f3edbfdb4c56bd6cd17899d220b64f0050c
gustavocdaguer <gustavocdaguer@gmail.com>
2022-04-19 00:34:31 -0300

    CRUD da tabela de produtos concluido

--- app/routes.js
+++ app/routes.js
@@ -4,5 +4,8 @@ const router = express.Router();
 const ProdutoController = require('./controllers/ProdutoController')
 
 router.get('/produtor/catalogo', ProdutoController.catalogarTodos);
+router.post('/produto', ProdutoController.inserir);
+router.put('/produto/:ID', ProdutoController.alterar);
+router.delete('/produto/:ID', ProdutoController.excluir);
 
 module.exports = router;
